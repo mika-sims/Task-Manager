405e039d2fea2f38c636af00e7346a3af462f114
Mikail Simsek <mikailsimsek.trb@gmail.com>
2023-01-20 09:37:59 +0000

    Import app and db variables from taskmanager folder

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,2 +1,5 @@
 # Import render_template function for template rendering
 from flask import render_template
+
+# Import app and db variables from main taskmanager folder
+from taskmanager import app, db
