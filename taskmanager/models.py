2fbfd797cfe8955fecfdf1587b31d40c70e148c4
Mikail Simsek <mikailsimsek.trb@gmail.com>
2023-01-20 10:02:18 +0000

    Add Category class for category model schema

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -1,2 +1,13 @@
 # Import db variable from taskmanager folder
 from taskmanager import db
+
+
+class Category(db.Model):
+    # Schema for the Category model
+    id = db.Column(db.Integer, primary_key=True)
+    category_name = db.Column(db.String(25), unique=True, nullable=False)
+    tasks = db.relationship("Task", backref="category", cascade="all, delete",lazy=True)
+    
+    def __repr__(self):
+    # Represent itself in the form of a string
+        return self.category_name
