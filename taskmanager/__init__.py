741a9491a036b3c1bd3d277d865891063ce08637
Mikail Simsek <mikailsimsek.trb@gmail.com>
2023-01-20 09:34:00 +0000

    Create app and db variables and specify app config variables

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -10,3 +10,13 @@ from flask_sqlalchemy import SQLAlchemy
 # Import environment variables
 if os.path.exists("env.py"):
     import env
+
+# Instance of Flask() class
+app = Flask(__name__)
+
+# Environment variables for app configuration
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+
+# Instance of SQLAlchemy() class
+db = SQLAlchemy(app)
