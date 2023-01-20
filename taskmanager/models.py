3e9e17e32f43731ca2ed7ca339124fdc55682e1a
Mikail Simsek <mikailsimsek.trb@gmail.com>
2023-01-20 10:02:59 +0000

    Add Task class for task model schema

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -11,3 +11,17 @@ class Category(db.Model):
     def __repr__(self):
     # Represent itself in the form of a string
         return self.category_name
+
+
+class Task(db.Model):
+    # Schema for the Task model
+    id = db.Column(db.Integer, primary_key=True)
+    task_name = db.Column(db.String(50), unique=True, nullable=False)
+    task_description = db.Column(db.Text, nullable=False)
+    is_urgent = db.Column(db.Boolean, default=False, nullable=False)
+    due_date = db.Column(db.Date, nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)
+    
+    def __repr__(self):
+    # Represent itself in the form of a string
+        return f"#{self.id}-Task:{self.task_name} | Urgent:{self.is_urgent}"
