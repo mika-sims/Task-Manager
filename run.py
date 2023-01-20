7bb2eff13231c6ed97086301036bca3e9d23dfa7
Mikail Simsek <mikailsimsek.trb@gmail.com>
2023-01-20 09:41:05 +0000

    Add if statement for checking the requirements to run the app

--- run.py
+++ run.py
@@ -3,3 +3,11 @@ import os
 
 # Import app variable from taskmanager folder
 from taskmanager import app
+
+# Run application
+if __name__ == '__main__':
+    app.run(
+        host=os.environ.get("IP"),
+        port=os.environ.get("PORT"),
+        debug=os.environ.get("DEBUG")
+    )
