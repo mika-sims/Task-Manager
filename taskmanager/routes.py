91e61d64196c76ce66be75c92a7fe56d91757fac
Mikail Simsek <mikailsimsek.trb@gmail.com>
2023-01-20 09:38:38 +0000

    Create app route and target the home() function

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -3,3 +3,8 @@ from flask import render_template
 
 # Import app and db variables from main taskmanager folder
 from taskmanager import app, db
+
+# App route 
+@app.route("/")
+def home():
+    return render_template("base.html")
