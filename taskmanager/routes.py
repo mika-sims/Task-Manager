69609ca5b4cf6957f159938780f01af7d4a42b95
Mikail Simsek <mikailsimsek.trb@gmail.com>
2023-01-20 10:07:00 +0000

    Import Category and Task classes

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -4,6 +4,9 @@ from flask import render_template
 # Import app and db variables from main taskmanager folder
 from taskmanager import app, db
 
+# Import models classes from models.py file
+from taskmanager.models import Category, Task
+
 # App route 
 @app.route("/")
 def home():
