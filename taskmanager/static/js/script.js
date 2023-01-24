3214ea86157bd09bdbd10eba5659c468d61de2e4
Mikail Simsek <mikailsimsek.trb@gmail.com>
2023-01-24 12:19:44 +0000

    Add function to initialize collapsible functionalty for tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -13,4 +13,8 @@ document.addEventListener('DOMContentLoaded', function () {
   // Select initialization
   let selects = document.querySelectorAll('select');
   M.FormSelect.init(selects);
+
+  // Collapsible initialization
+  let collapsibles = document.querySelectorAll('.collapsible');
+  M.Collapsible.init(collapsibles);
 });
