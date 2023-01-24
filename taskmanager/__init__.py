28db7d6963ea0c640fad08cdc7b4a38369bbc363
Mikail Simsek <mikailsimsek.trb@gmail.com>
2023-01-24 13:42:39 +0000

    Edit environment variables for app configuration

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -16,7 +16,17 @@ app = Flask(__name__)
 
 # Environment variables for app configuration
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+if os.environ.get("DEVELOPMENT") == "True":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+    if os.environ.get("DEVELOPMENT") == "True":
+     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+    else:
+        uri = os.environ.get("DATABASE_URL")
+        if uri.startswith("postgres://"):
+            uri = uri.replace("postgres://", "postgresql://", 1)
+        app.config["SQLALCHEMY_DATABASE_URI"] = uri
 
 # Instance of SQLAlchemy() class
 db = SQLAlchemy(app)
