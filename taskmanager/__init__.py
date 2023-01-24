67b3ce36e3717e09c7a714821e8f24d5e6c650e3
Mikail Simsek <mikailsimsek.trb@gmail.com>
2023-01-24 16:20:16 +0000

    Edit environment variables for app configuration

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -17,18 +17,13 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 
 # Environment variables for app configuration
-app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 if os.environ.get("DEVELOPMENT") == "True":
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")  # local
 else:
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
-    if os.environ.get("DEVELOPMENT") == "True":
-     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
-    else:
-        uri = os.environ.get("DATABASE_URL")
-        if uri.startswith("postgres://"):
-            uri = uri.replace("postgres://", "postgresql://", 1)
-        app.config["SQLALCHEMY_DATABASE_URI"] = uri
+    uri = os.environ.get("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri  # heroku
 
 # Instance of SQLAlchemy() class
 db = SQLAlchemy(app)
