8e4f1e8b0b28ffe6846b179d6909efc43ec223d5
Mikail Simsek <mikailsimsek.trb@gmail.com>
2023-01-24 12:25:29 +0100

    Initialize datepicker function

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Materialize css sidenav initialization
-    let sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav);
+document.addEventListener('DOMContentLoaded', function () {
+  // Materialize css sidenav initialization
+  let sidenav = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(sidenav);
+
+  // Datepicker initialization
+  var datepicker = document.querySelectorAll('.datepicker');
+  M.Datepicker.init(datepicker, {
+    format: "dd mmmm, yyyy",
+    i18n: { done: "Select" }
   });
+});
