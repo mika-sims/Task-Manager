e63520b215c358c9e6160a504e80e1bc9609cd39
Mikail Şimşek <77218306+mika-sims@users.noreply.github.com>
2025-03-10 15:58:21 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,13 +1,9 @@
-# Import OS module
 import os
-
-# Import app variable from taskmanager folder
 from taskmanager import app
 
-# Run application
 if __name__ == "__main__":
     app.run(
-        host=os.environ.get("IP"),
-        port=int(os.environ.get("PORT")),
-        debug=os.environ.get("DEBUG")
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", 10000)),
+        debug=os.environ.get("DEBUG", "False") == "True"
     )
